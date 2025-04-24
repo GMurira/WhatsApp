3c4c0b71614898163951e8aba721f75f01bd89d7
GMurira <nguyaimurira@gmail.com>
2025-04-24 14:37:26 +0300

    Working With Drop down menu:

--- app/src/main/java/com/example/whatsapp/presentation/screens/UserRegistrationScreen.kt
+++ app/src/main/java/com/example/whatsapp/presentation/screens/UserRegistrationScreen.kt
@@ -9,9 +9,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.HorizontalDivider
@@ -29,11 +33,13 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.whatsapp.R
 
 /**
  * This is a screen where new users are prompted to create an account on the platform
@@ -92,7 +98,10 @@ fun UserRegistration(modifier: Modifier = Modifier) {
             modifier = Modifier.padding(horizontal = 66.dp), thickness = 2.dp, color = Color.Green
         )
         DropdownMenu(
-            expanded = expanded, onDismissRequest = { expanded = false }) {
+            expanded = expanded,
+            onDismissRequest = { expanded = false },
+            modifier = Modifier.fillMaxWidth()
+        ) {
             listOf("Kenya", "Uganda", "Tanzania", "Ethiopia").forEach { country ->
                 DropdownMenuItem(text = { Text(text = country) }, onClick = {
                     selectedCountry = country
@@ -138,6 +147,15 @@ fun UserRegistration(modifier: Modifier = Modifier) {
             }
             Spacer( modifier = Modifier.height(16.dp))
             Text( text = "Charges may apply", fontSize = 12.sp)
+            Spacer(modifier = Modifier.height(45.dp))
+            Button(
+                onClick = {},
+                modifier = Modifier.size(280.dp, 43.dp),
+                shape = RoundedCornerShape(6.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = Color.Green)
+            ) {
+                Text( text  = stringResource(R.string.Next_button), fontSize = 18.sp)
+            }
         }
 
     }
